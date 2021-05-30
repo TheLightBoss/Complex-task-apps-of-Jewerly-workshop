cd4d0e777b15cfa58caaa6fa1e0228861b6c391e
saakiys <saak4ik@gmail.com>
2021-05-31 00:04:56 +0300

    Final Version CentrServer

---
+++ Web/ExampleHttp/Izdelie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ExampleHttp
+{
+    public partial class Izdelie
+    {
+        public Izdelie()
+        {
+            Izgotovlenies = new HashSet<Izgotovlenie>();
+            Zakazs = new HashSet<Zakaz>();
+        }
+
+        public int IdIzd { get; set; }
+        public decimal PriceIzd { get; set; }
+        public int IdProb { get; set; }
+        public DateTime? DateShtampa { get; set; }
+        public int IdIzdspr { get; set; }
+        public double? Razmer { get; set; }
+
+        public virtual IzdelieSpr IdIzdsprNavigation { get; set; }
+        public virtual ICollection<Izgotovlenie> Izgotovlenies { get; set; }
+        public virtual ICollection<Zakaz> Zakazs { get; set; }
+    }
+}
